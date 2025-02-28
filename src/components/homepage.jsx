a07a814e22db1b57003f5c6018602a64b0d08063
manvithmp <manvithmp13@gmail.com>
2025-02-28 21:40:02 +0530

    newcommit3

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -118,7 +118,7 @@ export default function HomePage() {
         </div>
       ) : (
         <div className="container">
-          <img src="/src/assets/home.png" alt="home" />
+          <img src="./public/home.png" alt="home" />
           <h1>Pocket Notes</h1>
           <p>Send and receive messages without keeping your phone online.</p>
           <p>Use Pocket Notes on up to 4 linked devices and 1 mobile phone</p>
