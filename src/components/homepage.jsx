89967168c6b8f876534654d967339d4c22540b16
manvithmp <manvithmp13@gmail.com>
2025-02-28 22:37:41 +0530

    newcommit06

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -123,7 +123,7 @@ export default function HomePage() {
           <h1>Pocket Notes</h1>
           <p>Send and receive messages without keeping your phone online.</p>
           <p>Use Pocket Notes on up to 4 linked devices and 1 mobile phone</p>
-          <p className="endtoend">
+          <p className="endtoend">&#128274;
         end-to-end encrypted
       </p>
         </div>
